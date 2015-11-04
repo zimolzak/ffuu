01b12f4d608d3c1f021856f6955c99d551796762
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-11-04 09:24:54 -0500

    Loop over i, j. Fails because we need to import copy maybe.

--- main.py
+++ main.py
@@ -25,7 +25,14 @@ mykey = open('apikey.txt', 'r').read().splitlines()[0]
 mycx = '001893756405173909803:zmyrda2qwcc'
 service = build("customsearch", "v1", developerKey=mykey)
 
-for query in ['ffffuuuu', 'fffuuu']:
-  res = service.cse().list(q=query, cx=mycx).execute()
-  c = int(res['searchInformation']['totalResults'])
-  print query + ', ' + str(c)
+n = 3 # Max number of Fs or Us.
+M = [[None] * (n+1)] * (n+1)
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        query = ("f" * i) + ("u" * j)
+        res = service.cse().list(q=query, cx=mycx).execute()
+        c = int(res['searchInformation']['totalResults'])
+        M[i][j] = c
+
+print M
