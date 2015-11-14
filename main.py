0dfb862794230628cf484c877c1a0002531781e2
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-11-14 08:26:33 -0500

    Extend along F axis to 20

--- main.py
+++ main.py
@@ -17,15 +17,18 @@
 """What is more common: FFFFUUUU, or FFUUUUU, or FFFFFUUU, etc.?"""
 
 from googleapiclient.discovery import build
+from time import sleep
+
 mykey = open('apikey.txt', 'r').read().splitlines()[0]
 mycx = '001893756405173909803:zmyrda2qwcc' #knowyourmeme + cheezburger + reddit
 service = build("customsearch", "v1", developerKey=mykey)
 
 #print "Fs,Us,count" #csv header
 
-for fs in range(1,16): # goal 23
-    for us in range(13,19): # goal 23
+for fs in range(16,21): # goal 23
+    for us in range(1,19): # goal 23
         query = ("f" * fs) + ("u" * us)
         res = service.cse().list(q=query, cx=mycx).execute()
         c = res['searchInformation']['totalResults']
         print str(fs) + "," + str(us) + "," + c
+        sleep(1)
