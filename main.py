cbfc0729c7c6745a551c87d8dc63fee13be26d50
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-11-13 23:39:26 -0500

    Slowly extend.

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ mykey = open('apikey.txt', 'r').read().splitlines()[0]
 mycx = '001893756405173909803:zmyrda2qwcc' #knowyourmeme + cheezburger + reddit
 service = build("customsearch", "v1", developerKey=mykey)
 
-print "Fs,Us,count" #csv header
+#print "Fs,Us,count" #csv header
 
-for fs in range(8,16):
-    for us in range(1,13):
+for fs in range(1,16): # goal 23
+    for us in range(13,19): # goal 23
         query = ("f" * fs) + ("u" * us)
         res = service.cse().list(q=query, cx=mycx).execute()
         c = res['searchInformation']['totalResults']
